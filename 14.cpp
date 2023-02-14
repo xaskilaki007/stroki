e3c7dc0d7237e416721072bc5dbb09fe4ed2fd37
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-02-14 22:01:40 +0300

    Create 14.cpp

---
+++ 14.cpp
@@ -0,0 +1,23 @@
+//14. Дана строка. Если она начинается на 'abc', то заменить их на 'www', иначе добавить в конец строки 'zzz'.
+
+#include <iostream>
+#include<string>
+
+void ReplaceStartChars(std::string &str)
+{
+    if(str.find("abc") == 0)
+ str.replace (0, 3, "www");
+    else
+ str += "zzz";
+}
+
+int main()
+{
+ std::string str;
+    std::getline(std::cin, str);
+
+    ReplaceStartChars(str);
+ std::cout << str;
+
+    0 0;
+}

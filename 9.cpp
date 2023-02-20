69606fa91317eee6a83f6e5fe46e0f20e2793447
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-02-20 20:01:26 +0300

    Update 9.cpp

--- 9.cpp
+++ 9.cpp
@@ -1,44 +1,45 @@
-//9 задание: По введенному номеру дня недели вывести его название, используя оператор множественного выбора. Нумерация дней недели начинается с 1 – понедельник, 2 — вторник и т. 
+//9 задание: По введенному номеру дня недели вывести его название, используя оператор множественного выбора. Нумерация дней недели начинается с 1 – понедельник, 2 — вторник и т.
 #include <iostream>
 #include <string>
 #include <array>
+using namespace std;
 int main(){
     int numb;
     string day;
-    std::cin»numb;
+    std::cin>>numb;
     if (numb>31)
-        std::cout«"Monday";
+        std::cout<<"Monday";
     else{
         while(numb>7){
             numb-=7;
         }
             switch(numb){
                 case 1:{
-                    std::cout«"Monday";
+                    std::cout<<"Monday";
                     break;
                 }
                 case 2:{
-                    std::cout«"Tuesday";
+                    std::cout<<"Tuesday";
                         break;
                 }
                 case 3:{
-                    std::cout«"Wednesday";
+                    std::cout<<"Wednesday";
                     break;
                 }
                 case 4:{
-                    std::cout«"Thursday";
+                    std::cout<<"Thursday";
                     break;
                 }
                 case 5:{
-                    std::cout«"Friday";
+                    std::cout<<"Friday";
                     break;
                 }
                 case 6:{
-                    std::cout«"Saturday";
+                    std::cout<<"Saturday";
                     break;
                 }
                 case 7:{
-                    std::cout«"Sunday";
+                    std::cout<<"Sunday";
                     break;
                 }
             }

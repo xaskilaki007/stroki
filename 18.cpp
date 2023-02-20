c72de7768bc83c6c1bcadba24e9178cb6ed31088
xaskilaki007 <115955213+xaskilaki007@users.noreply.github.com>
2023-02-20 19:56:42 +0300

    Update 18.cpp

--- 18.cpp
+++ 18.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
-    int main(){
- 	std::string text;
-	int k;
-	std::getline(std::cin,text);
- 	std::cin»k;
-	k-=1;
-	text.erase(k-1,1);
-	std::cout«text<<endl;
-	return 0;
-    }
+
+int main()
+{
+    string text;
+    int k;
+    std::getline(std::cin,text);
+    std::cin>>k;
+    k-=1;
+    text.erase(k,1);
+    cout<<text<<endl;
+    return 0;
+}
